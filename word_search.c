f4ecd4f83f1a7e60ed3f792d30b3e0fed0b934b6
zhiweijiang <499517692@qq.com>
2011-04-10 20:24:45 +0800

    use strncmp funtion

--- word_search.c
+++ word_search.c
@@ -1,52 +1,39 @@
 #include <stdio.h>
 #include <string.h>
+ 
 
-
-void word_search(char *p, char *q);
+int  word_search(char *p, char *q);
 
 int main(int argc, const char *argv[])
 {
  
     char Article[50]="This is a word search function";
-    char word[10];
+    char word[10]="word";
+    int num = word_search(Article,word);
 
-    puts("Please inpuot a word of you want to search:");
-    scanf("%s",word); 
-    word_search(Article, word);
+    if(num < 0)
+    {
+        printf("No such word\n");
+    }
+    else
+    {
+        printf("The position:%d\n",num);
+    }
     return 0;
 }
 
 
-void word_search(char *p, char *q)
+int word_search(char *p, char *q)
 {
  
-    char *p1=p;
-    char *q1=q;
     int i = 0;
-    int j = 0;
-
-    while( *p !='\0')
-        {
-            j= 0;
-            p1=p;
-            q1=q;
-            while(*q1!='\0')
-            {
-             if(*p1 == *q1)
-             j++;
-             else
-             break;
-             if(j+1==strlen(q))
-             {
-             printf("The word Position in the article:%d\n",i);
-            return;
-             }
-            q1++;
-            }
-            p++;
-            i++;
-        }
-        if(*p == '\0')
-            printf("Search no the word in the article!!\n");
+    while(*p != '\0')
+   {
+    if(strncmp( p, q,strlen(q)-1) == 0)
+        return i;
+      p++;
+      i++;
+   }
+   return -1;
 } 
     
