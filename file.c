85b08d9b7e5c51bd1354af6b3584339df60b1c4f
zhiweijiang <499517692@qq.com>
2011-04-20 18:53:38 +0800

    use fprintf and fscanf in file

--- file.c
+++ file.c
@@ -6,16 +6,18 @@ int main(int argc, const char *argv[])
     FILE *fp;
     char *str = "hello!";
     char str_1[20];
-
-    fp = fopen("text2", "w+");
+    int number[20] = {1,2,3,4,5};
+    int number_1[20];
+    fp = fopen("text2", "r+");
     if(!fp)
     {
         perror("open");
         exit(0);
     }
-//  fprintf(fp, "%s\n", str);
-    fscanf(fp, "%s", str_1);
-    printf("str_1:%s\n", str_1);
+    fprintf(fp, "%s %d %d\n", str, number[0], number[1]);
+    rewind(fp);
+    fscanf(fp, "%s%d%d", str_1, &number_1[0], &number_1[1]);
+    printf("str_1:%s %d %d\n", str_1, number_1[0], number[1]);
     fclose(fp);
 
     return 0;
