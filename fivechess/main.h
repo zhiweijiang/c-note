dddb4e205a96f7332f08fa9912f8d0b9a83b081f
zhiweijiang <499517692@qq.com>
2011-04-23 11:57:12 +0800

    Gobang

---
+++ fivechess/main.h
@@ -0,0 +1,18 @@
+#ifndef _MAIN_H_
+#define _MAIN_H_
+
+typedef unsigned int u32_t;
+typedef unsigned char u8_t;
+
+typedef struct
+{
+    int w;
+    int h;
+    int bbp;
+
+    void  *memo;
+}fbscr_t;
+
+int init_screen(void);
+
+#endif
