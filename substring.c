32fedb68352c675170b062f99818a995e7fa83bd
Zhiwei Jiang <499517692@qq.com>
2011-05-01 20:36:13 +0800

    a little modify for substring.c

--- substring.c
+++ substring.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <string.h>
 
 void longest_substring(char *src, char *dest);
 
@@ -10,9 +11,15 @@ int main(int argc, const char *argv[])
     puts("Plese input two string:");
     fgets(str,20,stdin);
     fgets(str1,20, stdin);
-    puts("the two string:");
-    printf("%s%s", str, str1);
-    longest_substring(str, str1);
+    if(strlen(str) >= strlen(str1))
+    {
+        longest_substring(str, str1);
+    }    
+    else
+    {
+        longest_substring(str1, str);
+    }
+
     return 0;
 }
 
@@ -25,25 +32,16 @@ void longest_substring(char *src, char *dest)
 
      for ( ; *src != '\n'; src++) 
      {
-        q = dest;
-         p = src;       
-             for(; *q != '\n'; q++)
+         for(q = dest, p = src; *q != '\n'; q++,p++)
+          {
+             if(*p == *q)
              {
-                while(*q != *p)q++;
-                 if(*p == *q)
-                 {
-                 length ++;printf("%d\n",length);
-                 p++;
-                 }
-                 else
-                 {
-                 length = 0;
-                 q--;
-                 p=src;
-                 }
-                 if(a<=length)
-                  a = length;
+              length ++;
              }
+          }
+         if(a <= length)
+           a = length;
+         length = 0;
      }
      printf("the longest public substring is:%d\n", a);
 
