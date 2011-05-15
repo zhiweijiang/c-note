1e5089bcd7fbed02d3da64fa1f357ec6b95b9cde
zhiweijiang <499517692@qq.com>
2011-05-15 09:37:25 +0800

    add wait timeout function

--- waittimeout.c
+++ waittimeout.c
@@ -2,14 +2,15 @@
 #include <unistd.h>
 #include <fcntl.h>
 #include <errno.h>
-#include <string.h>
 #include <stdlib.h>
-#define MSG_TRY "try agin\n"
+#include <string.h>
+#define MSG_TRY "try again\n"
+#define MSG_TIMEOUT "timeout\n"
 
 int main(int argc, const char *argv[])
 {
     char buf[10];
-    int fd, n;
+    int fd, n, i;
 
     fd = open("/dev/tty", O_RDONLY | O_NONBLOCK);
     if(fd < 0)
@@ -17,21 +18,24 @@ int main(int argc, const char *argv[])
         perror("open /dev/tty");
         exit(1);
     }
-    tryagin:
+    for (i = 0; i < 5; i++) 
+    {
         n = read(fd, buf, 10);
-        if(n < 0)
+        if(n >= 0)
+            break;
+        if(errno != EAGAIN)
         {
-            if(errno == EAGAIN)
-            {
-                sleep(1);
-                write(STDOUT_FILENO, MSG_TRY, strlen(MSG_TRY));
-                goto tryagin;
-            }
             perror("read /dev/tty");
             exit(1);
         }
+        sleep(1);
+        write(STDOUT_FILENO, MSG_TRY, strlen(MSG_TRY));
+    }
+    if(5 == i)
+        write(STDOUT_FILENO, MSG_TIMEOUT, strlen(MSG_TIMEOUT));
+    else 
         write(STDOUT_FILENO, buf, n);
-        close(fd);
+    close(fd);
 
     return 0;
 }
