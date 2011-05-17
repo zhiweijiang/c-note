8010441c52ed2538aed78e514a2263e844e8af1a
Zhiwei Jiang <499517692@qq.com>
2011-05-17 16:38:52 +0800

    some mouse operation

---
+++ FrameBuffer/mouseop.c
@@ -0,0 +1,243 @@
+#include "common.h"
+
+typedef struct
+{
+    int dx; 
+    int dy;
+    int dz;
+    int button;
+}mevent_t;
+
+static u32_t save_cursor[C_WIDTH*C_HIGHT];
+
+
+extern int mouse_open(const char *mdev);
+extern int mouse_parse(int fd, mevent_t * mevent);
+
+extern int mouse_draw(const pinfo_t fb, int x, int y);
+extern int mouse_restore(const pinfo_t fb, int x, int y);
+
+extern int mouse_test(pinfo_t fb);
+
+
+int init_fb(pinfo_t fb);
+int fb_pixel(pinfo_t fb, int x, int y, u32_t color);
+
+static u32_t cursor_pixel[C_WIDTH * C_HIGHT] = 
+{
+    BORD,T___,T___,T___,T___,T___,T___,T___,T___,T___,
+    BORD,BORD,T___,T___,T___,T___,T___,T___,T___,T___,  
+    BORD,X___,BORD,T___,T___,T___,T___,T___,T___,T___,
+    BORD,X___,X___,BORD,T___,T___,T___,T___,T___,T___,
+    BORD,X___,X___,X___,BORD,T___,T___,T___,T___,T___,
+    BORD,X___,X___,X___,X___,BORD,T___,T___,T___,T___,
+    BORD,X___,X___,X___,X___,X___,BORD,T___,T___,T___,
+    BORD,X___,X___,X___,X___,X___,X___,BORD,T___,T___,
+    BORD,X___,X___,X___,X___,X___,X___,X___,BORD,T___,
+    BORD,X___,X___,X___,X___,X___,X___,X___,X___,BORD,
+    BORD,X___,X___,X___,X___,X___,BORD,BORD,BORD,BORD,
+    BORD,X___,X___,BORD,X___,X___,BORD,T___,T___,T___,
+    BORD,X___,BORD,T___,BORD,X___,X___,BORD,T___,T___,
+    BORD,BORD,T___,T___,BORD,X___,X___,BORD,T___,T___,
+    T___,T___,T___,T___,T___,BORD,X___,X___,BORD,T___,
+    T___,T___,T___,T___,T___,BORD,X___,X___,BORD,T___,
+    T___,T___,T___,T___,T___,T___,BORD,BORD,T___,T___
+};
+
+
+int init_fb(pinfo_t fb)
+{
+    int fd;
+
+    if((fd = open("/dev/fb0", O_RDWR)) < 0)
+    {
+        perror("fb0");
+        return -1;
+    }
+
+    struct fb_var_screeninfo fb_var;
+
+    if((ioctl(fd, FBIOGET_VSCREENINFO, &fb_var) < 0))
+    {
+        perror("ioctl");
+        return -1;
+    }
+    fb->w = fb_var.xres;
+    fb->h = fb_var.yres;
+    fb->bpp = fb_var.bits_per_pixel;
+    printf("w:%d\th:\tbpp:%d\n", fb->w, fb->h, fb->bpp);
+
+    fb->fbmem = mmap(NULL, ((fb->w)*(fb->h)*(fb->bpp)/8), PROT_READ | PROT_WRITE, MAP_SHARED, fd, 0);
+    if(fb->fbmem == MAP_FAILED)
+    {
+        perror("mmap");
+        return -1;
+    }
+
+    close(fd);
+
+    return 0;
+}
+
+int fb_pixel(pinfo_t fb, int x, int y, u32_t color)
+{
+    u32_t *p;
+
+    p = (u32_t *) ((char *)fb->fbmem +((fb->w*y+x)*fb->bpp)/8);
+    *p = color;
+
+    return 0;
+}
+int mouse_test(pinfo_t fb)
+{
+    int fd;
+    mevent_t mevent;
+    int m_x = fb->w/2;
+    int m_y = fb->h/2;
+
+
+    if((fd = mouse_open("/dev/input/mice")) < 0)
+    {
+        perror("open");
+        exit(1);
+    }
+    mouse_draw(fb, m_x, m_y);
+
+    u8_t buf[] = {0xf3,0xc8,0xf3,0x64,0xf3,0x50};
+
+    if(write(fd, buf, sizeof(buf)) < sizeof(buf))
+    {
+        perror("write");
+        fprintf(stderr, "鼠标将不知支持滚轮\n");
+    }
+
+    while(1)
+    {
+        if(mouse_parse(fd, &mevent) == 0)
+        {
+            printf("dx = %d\tdy = %d\tdz = %d\t", mevent.dx, mevent.dy, mevent.dz);
+
+            mouse_restore(fb, m_x, m_y);
+
+            m_x += mevent.dx;
+            m_y += mevent.dy;
+
+            mouse_draw(fb, m_x, m_y);
+            printf("mx = %d\tmy = %d\n", m_x, m_y);
+            
+            switch(mevent.button)
+            {
+                case 1:
+                    printf("lelf button\n");
+                    break;
+                case 2:
+                    printf("right button\n");
+                    break;
+                case 4:
+                    printf("middle button\n");
+                    break;
+                case 0:
+                    printf("no button\n");
+                    break;
+                default:
+                    break;
+            }
+        }
+            else
+                ;
+    }
+    close(fd);
+    return 0;
+}
+
+
+int mouse_open(const char *mdev)
+{
+      if(mdev == NULL)
+          mdev = "/dev/input/mice";
+      return(open(mdev, O_RDWR | O_NONBLOCK)); 
+}
+   
+#define READ_MOUSE 8
+
+int mouse_parse(int fd, mevent_t *mevent)
+{
+      u8_t buf[READ_MOUSE];
+      int n;
+        
+      if((n = read(fd, buf, READ_MOUSE)) > 0)
+      {
+          mevent->button = buf[0]&0x07;
+          mevent->dx = buf[1];
+          mevent->dy = buf[2];
+          mevent->dz = buf[3];
+      }
+      else
+          return -1;
+  
+  return 0;
+}
+
+
+static int mouse_save(const pinfo_t fb, int x, int y)
+{
+    int i,j;
+
+    for (i = 0; i < C_WIDTH; i++) 
+    {
+        for (j = 0; j < C_HIGHT; j++)
+        {
+            save_cursor[i+j*C_WIDTH] = *(u32_t *)(fb->fbmem+((x+i)+(y+j)*fb->w)*fb->bpp/8);
+        }
+
+        return 0;
+    }
+}
+
+int mouse_draw(const pinfo_t fb, int x, int y)
+{
+    int i,j;
+
+    mouse_save(fb, x, y);
+
+    for (i = 0; i < C_WIDTH; i++) 
+    {
+        for (j = 0; j < C_HIGHT; j++)
+        {
+            if(cursor_pixel[i+j*C_WIDTH] != T___)
+                fb_pixel(fb, x+i, y+j, cursor_pixel[i+j*C_WIDTH]);
+        }
+    }
+
+    return 0;
+}
+
+int mouse_restore(const pinfo_t fb, int x, int y)
+{
+    int i, j;
+
+    for (i = 0; i < C_WIDTH; i++) 
+    {
+        for (j = 0; j < C_HIGHT; j++)
+        {
+            if(cursor_pixel[i+j*C_WIDTH] != T___)
+                fb_pixel(fb, x+i, y+j, save_cursor[i+j*C_WIDTH]);
+        }
+    }
+    
+    return 0;
+
+}
+
+int main(int argc, const char *argv[])
+{   
+    fb_info fb;
+
+    if(init_fb(&fb) < 0)
+    {
+        perror("init_fb");
+        exit(1);
+    }
+    mouse_test(&fb);
+    return 0;
+}
